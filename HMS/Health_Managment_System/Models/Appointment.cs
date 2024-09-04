1d7859500ca98ab74fde0433a9a0996c67e002ef
Matus Klecka <klecka.matus@gmail.com>
2024-09-04 12:02:32 +0200

    Testing

--- HMS/Health_Managment_System/Models/Appointment.cs
+++ HMS/Health_Managment_System/Models/Appointment.cs
@@ -5,7 +5,7 @@ namespace HealthcareManagementSystem.Models
     public class Appointment
     {
         public int Id { get; set; }
-        public int UserId { get; set; } // Link to the user
+        public int UserId { get; set; } // Link  to the user
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public string Status { get; set; } // Status of the appointment (e.g., Scheduled, Completed, Cancelled)
